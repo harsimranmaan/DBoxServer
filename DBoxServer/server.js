efdee5f451f99a4f13cb754588d339aa3a766c79
Harsimran Singh Maan <maan.harry@gmail.com>
2013-07-03 13:34:06 -0700

    Add socket

--- DBoxServer/server.js
+++ DBoxServer/server.js
@@ -3,5 +3,6 @@ http.createServer(function (req, res)
 {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(80, '127.0.0.1');
+}).listen(80);
+
 console.log('Server running at http://127.0.0.1:80/');
